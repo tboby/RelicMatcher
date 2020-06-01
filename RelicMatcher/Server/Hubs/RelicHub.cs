b38b71ca3b15be46afa52790e9c1b0c803c29184
Thomas Boby <th.boby@gmail.com>
2020-06-01 14:25:33 +0100

    Add project files.

---
+++ RelicMatcher/Server/Hubs/RelicHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using RelicMatcher.Shared;
+
+namespace RelicMatcher.Server.Hubs
+{
+    public class RelicHub : Hub
+    {
+        private static List<RelicType> CurrentRelics = new List<RelicType>();
+
+        public async Task AddRelic(string user, RelicType relicType)
+        {
+            CurrentRelics.Add(relicType);
+            await Clients.All.SendAsync("ReceiveRelicList", CurrentRelics);
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("ReceiveRelicList", CurrentRelics);
+            await base.OnConnectedAsync();
+        }
+    }
+}
---
+++ RelicMatcher/Shared/RelicTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelicMatcher.Shared
+{
+    public enum RelicType {
+        A1,
+        A2,
+        A3,
+        A4
+    }
+}
